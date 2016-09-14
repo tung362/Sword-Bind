f792987bd756840b0bb218bdda2af5a9b6af1274
Tung_Nguyen <7319tnguyen040@gmail.com>
2016-09-13 19:31:53 -0700

    Partially created sword slash effect system

--- Assets/RandomValueOnEndOfAnimation.cs
+++ Assets/RandomValueOnEndOfAnimation.cs
@@ -23,8 +23,13 @@ public class RandomValueOnEndOfAnimation : StateMachineBehaviour
 
     [Header("Settings")]
     private bool RunOnce = true;
-    public float StartingAnimationEndValue = 1;
     public float RestrictedNumber = 0;
+    private float StartingAnimationEndValue = 1;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        StartingAnimationEndValue = 1;
+    }
 
     override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
